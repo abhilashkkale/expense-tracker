aea5049be74ed1bc9d7940957758e4c980c05998
Abhilash Kale <abhilash.kale@go-jek.com>
2021-05-13 00:31:19 +0530

    Extracted the common card style into separate common component Card and used that as wrapper for Expenses and ExpenseItem component using children props - [ Composition in react ]

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,16 +1,17 @@
 import './ExpenseItem.css';
 import ExpenseDate from '../ExpenseDate/ExpenseDate';
+import Card from '../common/Card/Card';
 
 // Here instead of props,  we can also name it as any variable name like say data.
 function ExpenseItem(props) {
   return (
-    <div className='expense-item'>
+    <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
-    </div>
+    </Card>
   );
 }
 
