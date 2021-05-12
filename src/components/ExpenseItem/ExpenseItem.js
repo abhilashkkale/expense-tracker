1d47fe3d9b095a9f86358d35f5f1a5de1f9a85f5
Abhilash Kale <abhilash.kale@go-jek.com>
2021-05-12 23:33:29 +0530

    Split date details on UI to be shown in readable format in english wordings using js's toLocaleString in-built function

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -2,9 +2,17 @@ import './ExpenseItem.css';
 
 // Here instead of props,  we can also name it as any variable name like say data.
 function ExpenseItem(props) {
+  const month = props.date.toLocaleString('en-US', { month: 'long' });
+  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
+  const year = props.date.getFullYear();
+
   return (
     <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
@@ -12,5 +20,6 @@ function ExpenseItem(props) {
     </div>
   );
 }
+
 
 export default ExpenseItem;
