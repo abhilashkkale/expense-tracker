64b045fadc4e76d203e5d55470a45d20eeaa778a
Abhilash Kale <abhilash.kale@go-jek.com>
2021-05-12 23:55:06 +0530

    Extract date details in separate ExpenseDate component and added styles for date details

--- src/components/ExpenseItem/ExpenseItem.js
+++ src/components/ExpenseItem/ExpenseItem.js
@@ -1,18 +1,11 @@
 import './ExpenseItem.css';
+import ExpenseDate from '../ExpenseDate/ExpenseDate';
 
 // Here instead of props,  we can also name it as any variable name like say data.
 function ExpenseItem(props) {
-  const month = props.date.toLocaleString('en-US', { month: 'long' });
-  const day = props.date.toLocaleString('en-US', { day: '2-digit' });
-  const year = props.date.getFullYear();
-
   return (
     <div className='expense-item'>
-      <div>
-        <div>{month}</div>
-        <div>{year}</div>
-        <div>{day}</div>
-      </div>
+      <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
@@ -20,6 +13,5 @@ function ExpenseItem(props) {
     </div>
   );
 }
-
 
 export default ExpenseItem;
