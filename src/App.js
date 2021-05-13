c08032d10ccdb65b0ecf865f5e225817a50145db
Abhilash Kale <abhilash.kale@go-jek.com>
2021-05-13 22:22:00 +0530

    Extracted code into ExpenseForm and NewExpense components from Expense and added two way data binding, Events : onClick, onSubmit, onChangeEvents, useState and ExpenseFilter component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Expenses from './components/Expenses/Expenses';
+import NewExpense from './components/NewExpense/NewExpense';
 const App = () => {
   const expenses = [
     {
@@ -23,6 +24,11 @@ const App = () => {
     },
   ];
 
+  const addExpenseHandler = expense => {
+    console.log('In App.js');
+    console.log(expense);
+  };
+
   // return React.createElement(
   //   'div',
   //   {},
@@ -32,7 +38,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>Let's get started!</h2>
+      <NewExpense onAddExpense={addExpenseHandler} />
       <Expenses items={expenses} />
     </div>
   );
