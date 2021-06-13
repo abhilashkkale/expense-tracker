35d77823a3e548a150d35e057871e8a873bb8596
Abhilash Kale <abhilash.kale@go-jek.com>
2021-06-13 23:11:12 +0530

    Added chart component which display the monthly expenses bar chart for the selected year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
-import ExpenseItem from '../ExpenseItem/ExpenseItem';
+import ExpensesList from '../ExpensesList/ExpensesList';
+import ExpensesChart from './ExpensesChart';
 import './Expenses.css';
 import Card from '../common/Card/Card';
 import ExpensesFilter from '../ExpensesFilter/ExpensesFilter';
@@ -15,19 +16,6 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString() === filteredYear;
   });
 
-  let expensesContent = <p>No expenses found.</p>;
-
-  if (filteredExpenses.length > 0) {
-    expensesContent = filteredExpenses.map((expense) => (
-      <ExpenseItem
-        key={expense.id}
-        title={expense.title}
-        amount={expense.amount}
-        date={expense.date}
-      />
-    ));
-  }
-
   return (
     <div>
       <Card className='expenses'>
@@ -35,7 +23,8 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        {expensesContent}
+        <ExpensesChart expenses={filteredExpenses} />
+        <ExpensesList items={filteredExpenses} />
       </Card>
     </div>
   );
